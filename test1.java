93044effca9cac877b6849892fd09a16f9d29921
seankenny13 <146842491+seankenny13@users.noreply.github.com>
2023-10-05 00:27:06 +0200

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,5 +1,9 @@
-public class helloworld {
+public class FirstTenNumbers {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Loop from 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i); // Print the current number
+        }
     }
 }
+
